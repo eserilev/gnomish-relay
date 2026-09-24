1c4af42b15e83cc642e558f055a6faf26c586e8f
Eitan Seri- Levi <eserilev@gmail.com>
2026-09-24 12:49:24 -0700

    Run cargo deny in check-all

--- scripts/check-all.sh
+++ scripts/check-all.sh
@@ -6,6 +6,7 @@ cd "$root"
 cargo fmt --check
 cargo clippy --all-targets -q -- -D warnings
 cargo test -q
+cargo deny --log-level error check
 stylua --check addon
 scripts/check-proofs.sh
 scripts/check-model.sh
